81f8bc26c5b23f322192c2ffa2e2732f6cc63fc4
Hammad Sheikh-Bin-Nadeem <hammadn788@gmail.com>
2022-01-09 02:30:02 +0500

    Updates

---
+++ precache-manifest.fbd2f1d7ad1ac9ed0a94d62a4da580e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1aaf2ef0e5155bef6f13c5d3c62fe0",
+    "url": "/universityPortal/index.html"
+  },
+  {
+    "revision": "1ff7ec29a714c89326d6",
+    "url": "/universityPortal/static/css/main.eaec06ba.chunk.css"
+  },
+  {
+    "revision": "e9f4ba7dca637614416e",
+    "url": "/universityPortal/static/js/2.8726c7d7.chunk.js"
+  },
+  {
+    "revision": "65565a788e925bcb83db5d19ed334278",
+    "url": "/universityPortal/static/js/2.8726c7d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff7ec29a714c89326d6",
+    "url": "/universityPortal/static/js/main.9b678649.chunk.js"
+  },
+  {
+    "revision": "b819ea93cef4fcb1068d",
+    "url": "/universityPortal/static/js/runtime-main.59f37a1d.js"
+  },
+  {
+    "revision": "b5fc66f4cadb2092fef824e5e9df1009",
+    "url": "/universityPortal/static/media/green-chameleon-s9CC2SKySJM-unsplash.b5fc66f4.jpg"
+  }
+]);
